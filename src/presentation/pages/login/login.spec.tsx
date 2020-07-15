2c0a799a2f9340f764393b240ff48da2c845febe
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-07-15 23:41:18 +0100

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,9 +6,15 @@ describe('Login Component', () => {
   test('should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
-    expect(errorWrap.childElementCount).toBe(0)
-
     const submitButton = getByTestId('submit') as HTMLButtonElement
+    const emailStatus = getByTestId('email-status')
+    const passwordStatus = getByTestId('password-status')
+
+    expect(errorWrap.childElementCount).toBe(0)
     expect(submitButton.disabled).toBe(true)
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('•')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('•')
   })
 })

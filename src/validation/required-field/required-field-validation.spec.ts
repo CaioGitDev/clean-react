e0903d2ba1ba3a59a1edf582e360cc405e42a20f
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-07-26 23:20:04 +0100

    test: ensure required field validation returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { RequiredFieldError } from '@/validation/erros'
+
+describe('RequiredFieldValidation', () => {
+  test('should return error if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('should return error if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+})

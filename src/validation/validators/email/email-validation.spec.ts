6e4a98ff7a42e745f637dcbe45bab06bbe9ea26b
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-07-27 22:44:23 +0100

    test: ensure emailvalidation return false if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/erros'
+import * as faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.random.words())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return false if email is valid', () => {
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })

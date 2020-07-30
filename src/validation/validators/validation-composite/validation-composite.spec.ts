0e9dca838e89fb923ddb7fd3045d4b5633fec3b1
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-07-30 21:53:40 +0100

    test: ensure validation composite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,6 +4,7 @@ import { FieldValidationSpy } from '../test/mock-field-validation'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('any_first_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
     fieldValidationSpy2.error = new Error('any_message')
     const sut = new ValidationComposite([
@@ -11,6 +12,6 @@ describe('ValidationComposite', () => {
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_message')
+    expect(error).toBe('any_first_message')
   })
 })

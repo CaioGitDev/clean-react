2469ac2f7958a7db5e7e60491fa0251555e90c9d
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-02 21:52:55 +0100

    test: ensure validation builder has a email helper

--- src/validation/validators/builder/validation-build.spec.ts
+++ src/validation/validators/builder/validation-build.spec.ts
@@ -1,9 +1,19 @@
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 import { ValidationBuilder } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('should return requiredfieldvalidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('should return requiredfieldvalidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
+  })
+
+  test('should return requiredfieldvalidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })

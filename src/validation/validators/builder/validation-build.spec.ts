64f9ebaedee3f8b7dc7c339cfe86ba8ffb9b3866
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-02 21:55:21 +0100

    feat: ensure validation builder has a min length helper

--- src/validation/validators/builder/validation-build.spec.ts
+++ src/validation/validators/builder/validation-build.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 import { ValidationBuilder } from './validation-builder'
 
 describe('ValidationBuilder', () => {
@@ -13,7 +13,7 @@ describe('ValidationBuilder', () => {
   })
 
   test('should return requiredfieldvalidation', () => {
-    const validations = ValidationBuilder.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const validations = ValidationBuilder.field('any_field').min(5).build()
+    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
 })

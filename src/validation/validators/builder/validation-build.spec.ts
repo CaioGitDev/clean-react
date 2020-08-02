e22bae98470c9fc366aa5109cf25731e751003aa
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-02 21:57:27 +0100

    fix: fix test names on validation builder

--- src/validation/validators/builder/validation-build.spec.ts
+++ src/validation/validators/builder/validation-build.spec.ts
@@ -7,13 +7,14 @@ describe('ValidationBuilder', () => {
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
   })
 
-  test('should return requiredfieldvalidation', () => {
+  test('should return EmailValidation', () => {
     const validations = ValidationBuilder.field('any_field').email().build()
     expect(validations).toEqual([new EmailValidation('any_field')])
   })
 
-  test('should return requiredfieldvalidation', () => {
+  test('should return MinLengthValidation', () => {
     const validations = ValidationBuilder.field('any_field').min(5).build()
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
+  
 })
